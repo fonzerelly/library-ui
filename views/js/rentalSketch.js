6d7a0c1e26bf5b2160cb702981b21f56e3427820
floragit <flora.eggers@ibm.com>
2017-05-10 17:46:47 +0200

    deleteallrentals and conversation changes

--- views/js/rentalSketch.js
+++ views/js/rentalSketch.js
@@ -240,114 +240,7 @@ function formToJSONRental() {
 		});
 }
 
-
-
-
-
-
-/*$(document).ready(function() {
-	showRentals();
-});
-
-function showRentals() {
-	$('#rentalsTableRows').empty();
-	$.ajax({
-			url: rootURLrental
-	}).then(function(data) {
-		$.each( data, function( key, val ) {
-			$('#rentalsTableRows').append(
-				'<tr> <td>'+val.id+'</td> <td>'+val.bookuri+'</td> <td>'+val.customeruri+'</td> <td class="dateTd">'+val.start+'</td> <td class="dateTd">'+val.end+'</td>'+
-				'<td>'+'<p><a class="btn btn-default returnBook" data-toggle="popover" data-placement="left" ><span class="glyphicon glyphicon-remove">'+
-				//'<td>'+'<a class="btn btn-default returnBook"><span class="glyphicon glyphicon-send">'+
-				'</span></a></p>'+
-				'<p><a class="btn btn-default updateRental" data-toggle="popover" data-placement="left">'+
-				'<span class="glyphicon glyphicon-pencil"></span></a></p></td>'+
-				' </tr>'
-			);
-			$('[class="btn btn-default returnBook"]').popover({
-				html: true,
-				trigger: 'click',
-				content: function () {
-					return '<div class="popoverContent"><p>Return this book?</p>'+
-					'<p></p><p><div>'+
-					'<button type="button" class="btn btn-default returnCheck">'+
-					'<span class="glyphicon glyphicon-ok"></button>'+
-					'&nbsp; <button type="button" class="btn btn-default returnNot">'+
-					'Cancel</button></div></p></div>';
-				}
-			});
-			$('[class="btn btn-default updateRental"]').popover({
-				html: true,
-				trigger: 'click',
-				content: function () {
-					return '<div class="popoverContent">'+
-					'<div class="col-sm-offset-2"><strong>Update Rental:</strong></div>'+
-					'<form class="form-horizontal">'+
-						'<div class="form-group">'+
-							'<label class="control-label" for="selBidUpdate">Book ID:</label>'+
-							'<select class="form-control rentalInput" id="selBidUpdate"></select>'+
-						'</div>'+
-						'<div class="form-group">'+
-							'<label class="control-label" for="selCidUpdate">Customer ID:</label>'+
-							'<select class="form-control rentalInput" id="selCidUpdate"></select>'+
-						'</div>'+
-						'<div class="form-group">'+
-							'<label class="control-label" for="startUpdate">Start Date:</label>'+
-							'<input type="text" class="form-control rentalInput" id="startUpdate" placeholder="YYYY-MM-DD">'+
-						'</div>'+
-							'<div class="form-group">'+
-								'<label class="control-label" for="endUpdate">End Date:</label>'+
-								'<input type="text" class="form-control rentalInput" id="endUpdate" placeholder="YYYY-MM-DD">'+
-							'</div>'+
-							'<div class="form-group">'+
-								'<button type="button" class="btn btn-default" id="updateRentalbtn">Update</button>'+
-								'&nbsp; <button type="button" class="btn btn-default updateNot">'+
-								'Cancel</button>'
-							'</div>'+
-						'</form>'+
-						'</div>';
-				}
-			});
-		});
-	});
-}
-*/
-
-
-
-//not needed
-/*
-$(document).on("click", ".updateRental", function (e) {
-	$('#selCidUpdate').empty();
-	$('#selBidUpdate').empty();
-	$('.customerTable tr').each(function (i, row) {
-		if(i!=0){
-			var $row = $(row);
-			var $tempCId = $row[0].children[0].innerHTML;
-			$('#selCidUpdate').append(
-				'<option>'+$tempCId+'</option>'
-			);
-		}
-	});
-	$('.bookTable tr').each(function (i, row) {
-		if(i!=0){
-			var $row = $(row);
-			var $tempBId = $row[0].children[0].innerHTML;
-			$('#selBidUpdate').append(
-				'<option>'+$tempBId+'</option>'
-			);
-		}
-	});
-})*/
-
-
-
-
-
-
-
-
-
+//still used
 $(document).ready(function(){
 	$('#deleteAllRentalsbtn').popover({
 		html: true,
@@ -355,7 +248,7 @@ $(document).ready(function(){
 		container: 'body',
 		content: function () {
 			return '<div class="popoverContent">'+
-			'<p>Are you sure you want to delete all registered borrowed books?</p>'+
+			'<p>Are you sure you want to return all your borrowed books?</p>'+
 			'<p></p><p><div class="col-sm-offset-3">'+
 			'<button type="button" class="btn btn-default removeAllRents">'+
 			'<span class="glyphicon glyphicon-ok"></button>'+
@@ -366,15 +259,6 @@ $(document).ready(function(){
 })
 
 $(document).on("click", ".removeNot", function (e) {
-	/*var elem, evt = e ? e:event;
-	if (evt.srcElement)  elem = evt.srcElement;
-	else if (evt.target) elem = evt.target;
-
-	var parent = elem.parentElement;
-	while(!(parent.className=='form-group')){
-		parent = parent.parentElement;
-	}
-	var butt = $(parent).find("button")[0];*/
 	var butt = document.getElementById('deleteAllRentalsbtn');
 	$(butt).trigger('click');
 })
@@ -382,15 +266,15 @@ $(document).on("click", ".removeNot", function (e) {
 $(document).on("click", ".removeAllRents", function (e) {
 	deleteAllRents()
 })
-
+//still used
 function deleteAllRents(){
 	console.log('deleteAll');
 	$.ajax({
 		type: 'DELETE',
-		url: rootURLrental,
+		url: rootURLrental+'/user/'+userCustomerId,
 		success: function(data, textStatur, jqXHR){
 			//location.reload();
-			showRentals();
+			showUserRentals();
 			$.bootstrapGrowl("All borrowed books have been returned.", {type:'success', delay: 2000});
 			var butt = document.getElementById('deleteAllRentalsbtn');
 			$(butt).trigger('click');
@@ -409,24 +293,6 @@ $(document).ready(function() {
 	});
 })
 
-/*function updateRental() {
-  console.log('updateRental');
-  $.ajax({
-    type: 'PUT',
-    contentType: 'application/json',
-    url: rootURLrental +'/' + $('#rentalId').val(),
-		dataType: "json",
-		data: formToJSONRental(),
-		success: function(data, textStatus, jqXHR){
-			location.reload();
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			alert('updateRental error: '+ textStatus);
-		}
-  });
-}*/
-
-
 
 
 
@@ -434,7 +300,8 @@ $(document).ready(function() {
 
 //outside of executeRental()
 //adds rentals from conversation
-function addRentalConv(bookid, startDate, endDate) {
+//customerid still in the input (matches conversation.js)
+function addRentalConv(bookid, custid, startDate, endDate) {
   console.log('addRental from chat');
   $.ajax({
     type: 'POST',
